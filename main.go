51b72fdfd0440171fd9154b1c3d4d67970ec2581
Yuvraj Chhetri <yuvichh01@gmail.com>
2022-04-04 23:25:11 +0530

    modified project

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/thedevaddam/renderer"
+	"github.com/thedevsaddam/renderer"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var rnd *renderer.Render
+var db *mgo.Database
+
+const (
+	hostName       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	collectionName string = "todo"
+	port           string = ":9000"
+)
+
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		Title     string        `bson:"title"`
+		Completed bool          `bson:"completed"`
+		CreatedAt time.Time     `bson:"createAt"`
+	}
+
+	todo struct {
+		ID        string    `json:"id"`
+		Titile    string    `json:"title"`
+		Completed string    `json:"completed"`
+		CreateAt  time.Time `json:"created_at"`
+	}
+)
+
+func init() {
+	rnd = renderer.New()
+	sess, err := mgo.Dial(hostName)
+	checkErr(err)
+	sess.SetMode(mgo.Monotonic, true)
+	db = sess.DB(dbName)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	stopChan := make(chan os.Signal)
+	signal.Notify(stopChan, os.Interrupt)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", homeHandler)
+	r.Mount("/todo", todoHandlers())
+
+	srv := &http.Server{
+		Addr:         port,
+		Handler:      r,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	go func() {
+		log.Println("Lsitening on port", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen:%\n", err)
+		}
+	}()
+
+	<-stopChan
+	log.Println("shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	srv.Shutdown(ctx)
+
+}
+
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+	return rg
+}
